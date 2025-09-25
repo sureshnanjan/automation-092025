29400d391e6e22efa45f251f5709ca13eb7a90b4
Ramakrishna <ramakrishna000444@gmail.com>
2025-09-25 10:52:41 +0530

    third commit

--- hello_selenium.py
+++ hello_selenium.py
@@ -5,4 +5,7 @@ mybroswer.get('https://the-internet.herokuapp.com/')
 assert 'Internet' in mybroswer.title
 title = mybroswer.find_element(By.TAG_NAME, 'h1')
 assert 'Welcome to the-internet' in title.text 
+
+sub_title = mybroswer.find_element(By.TAG_NAME, 'h2')
+assert 'Available Examples' in sub_title.text 
 mybroswer.quit()
